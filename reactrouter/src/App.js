fe542425926ff3f32c7d28134bbbab3a3d6065ae
Jake Kaplan <kaplanjt@gmail.com>
2021-11-02 22:12:48 -0700

    nav added, functioning

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -5,10 +5,12 @@ import {Route} from "react-router-dom";
 import Stocks from "./pages/stocks";
 import Main from "./pages/main";
 import About from "./pages/about";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Route path="/">
         <Main />
       </Route>
