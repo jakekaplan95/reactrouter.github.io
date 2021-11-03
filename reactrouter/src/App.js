fe3f3acf018121eec7131ac490385fd6666d7d0d
Jake Kaplan <kaplanjt@gmail.com>
2021-11-02 20:02:59 -0700

    pages, components filled out, nav.js

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+// Import route and components
+import {Route} from "react-router-dom";
+import Stocks from "./pages/stocks";
+import Main from "./pages/main";
+import About from "./pages/about";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/">
+        <Main />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/stocks">
+        <Stocks />
+      </Route>
     </div>
   );
 }
